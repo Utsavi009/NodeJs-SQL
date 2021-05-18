ac632e41418faaabc1cac5dec43c42b2f0deb61b
Utsavi009 <utsavikathiriya@gmail.com>
2021-05-18 17:15:09 +0200

    final project

--- conf/conf.js
+++ conf/conf.js
@@ -1,16 +1,13 @@
 const mysql = require('mysql2');  //npm i mysql2
 
-require('dotenv').config;  // npm i dotenv
+require('dotenv').config();  // npm i dotenv
 
 const connection = mysql.createConnection({
-    /* host : process.env.DB_HOSTNAME,
+
+    host : process.env.DB_HOSTNAME,
     user : process.env.DB_USERNAME,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME   */
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'myfirstdb' 
+    database : process.env.DB_NAME 
 })
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');  // npm i express
 const app = express();
-const PORT = 3001;
+require('dotenv').config();
+const PORT = process.env.PORT;
 const order = require('./routes/order');
 const user = require('./routes/user')
 
@@ -10,5 +11,5 @@ app.use('/', order);
 app.use('/', user);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 })
